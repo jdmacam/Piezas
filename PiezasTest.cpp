4d10b42e79a74af2ebd0b1df33208dad8d59efd4
Joseph Macam <jdmacam@gmail.com>
2020-05-06 12:32:07 -0700

    added test for entire board

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -19,9 +19,18 @@ TEST(PiezasTest, sanityCheck)
 	ASSERT_TRUE(true);
 }
 
-TEST(PiezasTest, constructor_test)
+TEST(PiezasTest, constructor_first_space)
 {
   Piezas obj;
   ASSERT_TRUE(obj.pieceAt(0,0) == Blank);
+}
 
+TEST(PiezasTest, constructor_all_spaces)
+{
+  Piezas obj;
+  for(int i = 0; i < BOARD_ROWS; i++){
+    for(int j = 0; j < BOARD_COLS; j++){
+      ASSERT_TRUE(obj.pieceAt(i,j) == Blank);
+    }
+  }
 }
