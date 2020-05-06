ffadf542f623e83a5a69e0474da46ce2ebd307d1
Joseph Macam <jdmacam@gmail.com>
2020-05-06 11:47:51 -0700

    Constructor implementation/testing

--- Piezas.cpp
+++ Piezas.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 /** CLASS Piezas
  * Class for representing a Piezas vertical board, which is roughly based
- * on the game "Connect Four" where pieces are placed in a column and 
+ * on the game "Connect Four" where pieces are placed in a column and
  * fall to the bottom of the column, or on top of other pieces already in
  * that column. For an illustration of the board, see:
  *  https://en.wikipedia.org/wiki/Connect_Four
@@ -17,11 +17,23 @@
 
 
 /**
- * Constructor sets an empty board (default 3 rows, 4 columns) and 
+ * Constructor sets an empty board (default 3 rows, 4 columns) and
  * specifies it is X's turn first
 **/
 Piezas::Piezas()
 {
+  //set size of the board
+  board.resize(BOARD_ROWS);
+  for(int i = 0; i < BOARD_ROWS; i++){
+    board[i].resize(BOARD_COLS);
+  }
+
+  //set all spaces to blank
+  for(int i = 0; i < BOARD_ROWS){
+    for(int j = 0; j < BOARD_COLS; j++){
+      board[i][j] = Blank;
+    }
+  }
 }
 
 /**
@@ -30,16 +42,22 @@ Piezas::Piezas()
 **/
 void Piezas::reset()
 {
+  //set all spaces to blank
+  for(int i = 0; i < BOARD_ROWS){
+    for(int j = 0; j < BOARD_COLS; j++){
+      board[i][j] = Blank;
+    }
+  }
 }
 
 /**
  * Places a piece of the current turn on the board, returns what
- * piece is placed, and toggles which Piece's turn it is. dropPiece does 
+ * piece is placed, and toggles which Piece's turn it is. dropPiece does
  * NOT allow to place a piece in a location where a column is full.
- * In that case, placePiece returns Piece Blank value 
+ * In that case, placePiece returns Piece Blank value
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
+**/
 Piece Piezas::dropPiece(int column)
 {
     return Blank;
@@ -57,7 +75,7 @@ Piece Piezas::pieceAt(int row, int column)
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
  * is not over, or Blank if the board is filled and no one has won ("tie").
- * For a game to be over, all locations on the board must be filled with X's 
+ * For a game to be over, all locations on the board must be filled with X's
  * and O's (i.e. no remaining Blank spaces). The winner is which player has
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -4,17 +4,28 @@
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
- 
+
 class PiezasTest : public ::testing::Test
 {
 	protected:
 		PiezasTest(){} //constructor runs before each test
 		virtual ~PiezasTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
 TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(PiezasTest, constructor_test)
+{
+  Piezas obj;
+  for(int i = 0; i < BOARD_COLS; i++){
+    for(int j = 0; j < BOARD_COLS; j++){
+      ASSERT_TRUE(board[i][j] == Blank);
+    }
+  }
+  
 }
